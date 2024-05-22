9d3c7966969a0a8af9a2395d0ddcef5e2580979a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-05-22 18:08:45 +0600

    create nav bar and footer

---
+++ front-end/src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <>
+      <Typography> this is home page</Typography>
+    </>
+  );
+};
+
+export default HomePage;
---
+++ front-end/src/components/shared/Navbar/Navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import Link from "next/link";
+import dynamic from "next/dynamic";
+
+const Navbar = () => {
+  //   const AuthButton = dynamic(
+  //     () => import("@/components/ui/AuthButton/AuthButton"),
+  //     { ssr: false }
+  //   );
+  return (
+    <Container>
+      <Stack
+        py={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Typography variant="h4" component={Link} href="/" fontWeight={600}>
+          P
+          <Box component="span" color="primary.main">
+            H
+          </Box>{" "}
+          Health Care
+        </Typography>
+
+        <Stack direction="row" justifyContent="space-between" gap={4}>
+          <Typography component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography>Health Plans</Typography>
+          <Typography>Medicine</Typography>
+          <Typography>Diagnostics</Typography>
+          <Typography>NGOs</Typography>
+        </Stack>
+
+        {/* <AuthButton /> */}
+      </Stack>
+    </Container>
+  );
+};
+
+export default Navbar;
