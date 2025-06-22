b960baa2a9391b1afb9b4387fc5b86c4f319c9e2
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2025-06-22 18:33:37 +0600

    fix server issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,29 @@
-import { Server } from "http"
-import app from "./app"
-import config from "./config"
-
-
+import { Server } from "http";
+import app from "./app";
+import config from "./config";
 
 async function main() {
-    const port = config.port
-    const server: Server = app.listen(port, () => {
-        console.log(`Server is running on ${port}`)
-    })
+  const port = config.port;
+  const server: Server = app.listen(port, () => {
+    console.log(`Server is running on ${port}`);
+  });
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.info("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+  process.on("uncaughtException", (error) => {
+    console.log(error);
+    exitHandler();
+  });
 
+  process.on("unhandledRejection", (error) => {
+    console.log(error);
+    exitHandler();
+  });
 }
 
-main()
-
+main();
